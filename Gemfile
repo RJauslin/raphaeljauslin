9489aa1df10877b1be14818caf33b7b31612a170
Rjauslin <jauslin.raphael@gmail.com>
2022-07-06 18:21:42 +0200

    remove scholar

--- Gemfile
+++ Gemfile
@@ -1,16 +1,17 @@
 source "https://rubygems.org"
 
-gem "github-pages", group: :jekyll_plugins
+gem "jekyll", "~> 3.9.2"
+gem "github-pages", "~> 226", group: :jekyll_plugins
+gem "minimal-mistakes-jekyll"
 
+gem "tzinfo", "~> 2.0.4"
 gem "tzinfo-data"
 gem "wdm", "~> 0.1.0" if Gem.win_platform?
 
 # If you have any plugins, put them here!
 group :jekyll_plugins do
   gem "jekyll-feed"
-  gem "jekyll-scholar"
   gem "jemoji"
 end
 
-gem "webrick", "~> 1.7"
-
+gem "webrick", "~> 1.7.0"
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,24 +2,24 @@ var store = [{
         "title": "Statistical Matching using Optimal Transport",
         "excerpt":"Introduction In this vignette, we will explain how some functions of the package are used to estimate a contingency table. We will work on the eusilc dataset of the laeken package. All the functions presented in the following are explained in the proposed manuscript by Raphaël Jauslin and Yves Tillé...","categories": [],
         "tags": [],
-        "url": "/matching/",
+        "url": "/raphaeljauslin/matching/",
         "teaser": null
       },{
         "title": "Wave Sampling",
         "excerpt":"Introduction Geographical data are generally auto-correlated. This is why it is preferable to avoid the measurement of neighboring units. We propose a new method for selecting spread samples from a finite spatial population with equal or unequal inclusion probabilities. The proposed method is called wave and stands for Weakly Associated...","categories": [],
         "tags": [],
-        "url": "/wave/",
+        "url": "/raphaeljauslin/wave/",
         "teaser": null
       },{
         "title": "Sequential balanced sampling",
         "excerpt":"Introduction In this vignette we are going to explain how to use the function balseq to select a balanced sample. Load data We will use the dataset belgianmunicipalties from the package sampling. The dataset does not contains any spatial coordinates. Luckily, a GEOjson is directly available from the https://hub.arcgis.com/datasets/esribeluxdata::belgium-municipalities-1/about. We...","categories": [],
         "tags": [],
-        "url": "/balseq/",
+        "url": "/raphaeljauslin/balseq/",
         "teaser": null
       },{
         "title": "Deville systematic",
-        "excerpt":"Introduction The Deville systematic design is a sampling design developed in 1998 by Jean-Claude Deville. The method has similarities with the systematic design but has different properties. (Chauvet, 2012) has shown that Deville systematic and ordered pivotal method are in fact the same underlying sampling design. This vignette shows how...","categories": [],
+        "excerpt":"Introduction The Deville systematic design is a sampling design developed in 1998 by Jean-Claude Deville. The method has similarities with the systematic design but has different properties. cha:12 has shown that Deville systematic and ordered pivotal method are in fact the same underlying sampling design. This vignette shows how to...","categories": [],
         "tags": [],
-        "url": "/deville/",
+        "url": "/raphaeljauslin/deville/",
         "teaser": null
       }]
